6ee1a03e8183b2fbe4847586dffe30e648f8dae4
Youngho Cho <dominoyh5@gmail.com>
2021-01-30 12:58:09 +0900

    prev rotate

---
+++ script.js
@@ -0,0 +1,42 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#EEEEEE";
+ctx.fillRect(0, 0, 500, 500);
+
+function drawArrow(degree) {
+  const radian = (degree / 180) * Math.PI;
+
+  const startX = 250;
+  const startY = 400;
+  const endX = 250 - 200 * Math.cos(radian);
+  const endY = 400 - 200 * Math.sin(radian);
+
+  // Arrow body
+  ctx.strokeStyle = "#EE3333";
+  ctx.lineWidth = 7;
+  ctx.beginPath();
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(endX, endY);
+  ctx.stroke();
+
+  // Arrow head
+  ctx.beginPath();
+  ctx.moveTo(endX, endY);
+  ctx.lineTo(endX, endY);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(endX, endY);
+  ctx.lineTo(endX, endY);
+  ctx.stroke();
+}
+
+ctx.translate(100, 0);
+ctx.rotate((10/ 180) * Math.PI);
+drawArrow(0);
+// drawArrow(45);
+// drawArrow(90);
+// drawArrow(135);
+// drawArrow(180);
+
